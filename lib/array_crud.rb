30cce8960d5e599cb4ad0473d7eb5bccf35e6c17
Jayson Kesterson <jayson.kesterson24@gmail.com>
2019-10-10 17:28:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = [carbink,gliscor,hawlucha,slowking]
+  array = ["carbink","gliscor","hawlucha","slowking"]
 end
 
 def add_element_to_end_of_array(array, element)
